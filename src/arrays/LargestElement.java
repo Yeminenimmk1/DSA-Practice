c5d336ad153837ecbbc2223fcdf56b0bbe0ac45f
Yeminenimmk1 <yeminenimmk@gmail.com>
2026-06-27 19:39:14 +0530

    first question in Arrays

--- src/arrays/LargestElement.java
+++ src/arrays/LargestElement.java
@@ -1,4 +1,18 @@
 package arrays;
 
 public class LargestElement {
+    public static void main(String[] args){
+        int[] arr={4,10,1,6,12,5,8,45,62,78,99,412,100,22,32,56,69,41};
+        int result=getLargest(arr);
+        System.out.println(result);
+    }
+    static int getLargest(int[] arr){
+        int lar=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > lar){
+                lar=arr[i];
+            }
+        }
+        return lar;
+    }
 }
